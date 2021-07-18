d5b4afd3b4f39e3f74f0ec549c17b1576e9e7e9b
Navdeep.K <navdeep152002@gmail.com>
2021-07-18 17:34:22 +0530

    Add selection sort on click

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Graph from "./Graph";
+var list = [];
+for(var i=0;i<10;i++){
+  list.push(Math.floor(Math.random()*500));
+}
+function App() { 
+  const [array,setarray] = useState(list);
+  const [elmnt1,setelmnt1] = useState(0);
+  const [elmnt2,setelmnt2] = useState(0);
+  
+  if(elmnt1!==array.length-1){
+    // setInterval(handleClick(elmnt1,elmnt2), 100);
+  }
+  function handleClick(elmnt1,elmnt2){
+    console.log(elmnt1,elmnt2,array[elmnt1],array[elmnt2]);
+    if(array[elmnt1]>array[elmnt2]){
+      var temp = array[elmnt1];
+      array[elmnt1] = array[elmnt2];
+      array[elmnt2] = temp;
+    }
+    if(elmnt2===array.length-1){
+      setelmnt1(elmnt1+1);
+      setelmnt2(elmnt1+1);
+    }
+    else{
+      setelmnt2(elmnt2+1);
+    }
+    setarray(arr => array);
+  }
 
-function App() {
+  
+  const ItemList = array.map((item,index) => <Graph height={String(item)}/>); 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div> 
+      {ItemList} 
+      <button onClick={() => { handleClick(elmnt1,elmnt2)}}>Next</button>  
+      
+    </div> 
   );
 }
 
---
+++ src/Graph.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Graph(props){
+    return <div className="bar" style={{height: String(props.height)+"px"}}>
+    </div>
+}
+
+export default Graph
