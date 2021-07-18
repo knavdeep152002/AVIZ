df4983e46ab28b9b8c0e4c3e476e751b302404bb
Navdeep.K <navdeep152002@gmail.com>
2021-07-18 21:47:49 +0530

    Add setinterval

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useState,useEffect  } from "react";
 import Graph from "./Graph";
 var list = [];
-for(var i=0;i<10;i++){
+for(var i=0;i<100;i++){
   list.push(Math.floor(Math.random()*500));
 }
 function App() { 
@@ -9,33 +9,35 @@ function App() {
   const [elmnt1,setelmnt1] = useState(0);
   const [elmnt2,setelmnt2] = useState(0);
   
-  if(elmnt1!==array.length-1){
-    // setInterval(handleClick(elmnt1,elmnt2), 100);
-  }
-  function handleClick(elmnt1,elmnt2){
+  function handleClick(){
+    if(elmnt1===array.length-1) return;
     console.log(elmnt1,elmnt2,array[elmnt1],array[elmnt2]);
     if(array[elmnt1]>array[elmnt2]){
       var temp = array[elmnt1];
       array[elmnt1] = array[elmnt2];
       array[elmnt2] = temp;
     }
-    if(elmnt2===array.length-1){
+    if(elmnt2===array.length-1 && elmnt1!==array.length-1){
       setelmnt1(elmnt1+1);
       setelmnt2(elmnt1+1);
     }
-    else{
+    else if(elmnt1!==array.length-1){
       setelmnt2(elmnt2+1);
     }
     setarray(arr => array);
   }
 
-  
+  useEffect(() => {
+    const interval = setInterval(() => {
+      handleClick()
+    }, 1);
+    return () => clearInterval(interval);
+  });
+
   const ItemList = array.map((item,index) => <Graph height={String(item)}/>); 
   return (
     <div> 
-      {ItemList} 
-      <button onClick={() => { handleClick(elmnt1,elmnt2)}}>Next</button>  
-      
+      {ItemList}       
     </div> 
   );
 }
